2dcd6bb82a6b219d40d5f2bbf3e99494bff0498a
Thippanakeerthireddy <thippanakeerthireddy@gmail.com>
2025-06-09 14:58:08 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,26 @@
+public class Recursion   
+
+{
+   
+ public void fun(int n)
+   
+ {
+        
+if(n==11) return;
+       
+ System.out.println(n);
+       
+ fun(n+1);
+    
+}
+	
+public static void main(String[] args) 
+{
+		
+Recursion m=new Recursion();
+		
+m.fun(1);
+	
+}
+
+}
