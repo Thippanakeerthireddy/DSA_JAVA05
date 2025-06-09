98ce1f3480f341b996597c4109b53a263d91e5fc
Thippanakeerthireddy <thippanakeerthireddy@gmail.com>
2025-06-09 14:58:59 +0530

    Add files via upload

---
+++ Recursionusingstring .java
@@ -0,0 +1,36 @@
+public class Recursionusingstring 
+
+{
+    
+public String fun(int n)
+    
+{
+        
+if(n==11) 
+        
+{
+        
+return "biryani";
+       
+ }
+        
+System.out.println(n);
+       
+ String s=fun(n+1);
+        
+System.out.println(n);
+        
+return s;
+    
+}
+	
+public static void main(String[] args)
+ {
+	
+Recursionusingstring m=new Recursionusingstring();
+	
+System.out.println(m.fun(1));
+	
+}
+
+}
