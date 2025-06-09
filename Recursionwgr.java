f0eff1f62e1b7b13345128a4e9ccf6e903e34cba
Thippanakeerthireddy <thippanakeerthireddy@gmail.com>
2025-06-09 14:59:25 +0530

    Add files via upload

---
+++ Recursionwgr.java
@@ -0,0 +1,27 @@
+public class Recursionwgr
+{
+    
+public void fun(int n)
+    
+{
+        
+if(n==11) return;
+       
+ System.out.println(n);
+        
+fun(n+1);
+        
+System.out.println(n);
+   
+ }
+	
+public static void main(String[] args)
+ {
+		
+Recursionwgr m=new Recursionwgr();
+		
+m.fun(1);
+	
+}
+
+}
