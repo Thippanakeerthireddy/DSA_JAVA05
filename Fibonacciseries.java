bca23c0703e07ee82c31e40eeb7d2a2e19b7688f
Thippanakeerthireddy <thippanakeerthireddy@gmail.com>
2025-06-09 14:57:50 +0530

    Add files via upload

---
+++ Fibonacciseries.java
@@ -0,0 +1,28 @@
+public class Fibonacciseries
+{
+    
+public int fun(int n)
+    
+{
+        
+if(n<=1) 
+       
+ {
+        
+return n;
+       
+ }
+        
+return fun(n-1)+fun(n-2);
+    
+}
+	
+public static void main(String[] args) 
+}
+Fibonacciseries m=new Fibonacciseries();
+	
+System.out.println(m.fun(5));
+	
+}
+
+}
