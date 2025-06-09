a7bb2bf95120cce48f1264f86045b3d2eba4207d
Thippanakeerthireddy <thippanakeerthireddy@gmail.com>
2025-06-09 14:59:45 +0530

    Add files via upload

---
+++ Rf.java
@@ -0,0 +1,30 @@
+public class Rf
+
+{
+    
+public int fun(int n)
+    
+{
+        
+if(n==1) 
+        
+{
+        
+return 1;
+      
+ }
+        
+return n*fun(n-1);
+   
+ }
+	
+public static void main(String[] args) 
+{
+	
+Rf m=new Rf();
+	
+System.out.println(m.fun(5));
+	
+}
+
+}
