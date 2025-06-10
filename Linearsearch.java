717181720805dcecc366ae142f4f2b0e0e3dc447
Thippanakeerthireddy <thippanakeerthireddy@gmail.com>
2025-06-10 14:54:34 +0530

    Add files via upload

--- Linearsearch.java
+++ Linearsearch.java
@@ -1,29 +1,44 @@
 import java.util.*;
+
 public class Linearsearch
+
 {
-	public static void main(String[] args) {
-	    int  [] a=new int[5];
-	    Scanner sc=new Scanner(System.in);
-	    for(int i=0;i<a.length;i++)
-	    {
-	        a[i]=sc.nextInt();
-	    }
-	    int n=sc.nextInt();
-	    int flag=1;
-	    for(int i=0;i<a.length;i++)
-	    {
-	        if(a[i]==n)
-	        {
-	            System.out.println("found at index" +i);
-	            flag=0;
-	            break;
-	        }
-	    }
-	    if(flag==1)
-	    {
-	        System.out.println("not found");
-	    }
-	    
-	}
 	
+public static void main(String[] args) 
+{
+	
+int [] a=new int[5];
+	
+Scanner sc=new Scanner(System.in);
+	
+for(int i=0;i<a.length;i++)
+	
+{
+	    
+a[i]=sc.nextInt();
+	
+}
+	
+int s=sc.nextInt();
+	
+for(int i=0;i<a.length;i++)
+	
+{
+	   
+ if(a[i]==5)
+	    
+{
+	       
+ System.out.println(i);
+	       
+ System.exit(0);
+	  
+}
+	
+}
+	
+System.out.println("not found");
+	
+}
+
 }
